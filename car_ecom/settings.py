622f6bea9333cc7e2675a6aa9cf4bf7081532d44
RaoulRV <raoulradulescu@gmail.com>
2022-11-13 16:28:21 +0000

    Removed secret key and changed debug

--- car_ecom/settings.py
+++ car_ecom/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37czw%b=g+!%3*)fd1^^i8x+e)v7bzpgvc5=h%t3zd@p=dhs-u'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['luxuryrentals.herokuapp.com', 'localhost']
 
