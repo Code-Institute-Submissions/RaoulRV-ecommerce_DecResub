648c506c9286c5558bb1f2d4ca1502854c4bf1a8
RaoulRV <raoulradulescu@gmail.com>
2022-11-14 09:05:11 +0000

    Added AWS Default setting and changed the media paths for logo and hero video

--- car_ecom/settings.py
+++ car_ecom/settings.py
@@ -185,6 +185,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
+    AWS_DEFAULT_ACL = None
     AWS_STORAGE_BUCKET_NAME = 'luxuryrentals'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
